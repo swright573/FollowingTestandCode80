a412dc28c029b78c0bd341546f5c8bb899f33248
swright <swright573@icloud.com>
2019-08-25 12:47:01 -0400

    Initial commit

---
+++ susancode.py
@@ -0,0 +1,16 @@
+"""
+Following along with TestandCode episode 80
+"""
+
+__version__ = '0.1'
+
+
+
+def outtext(sentence):
+    print(sentence)
+
+def main():
+    outtext('Hello world ... Susan here.')
+
+if __name__ == "__main__":
+    main()
