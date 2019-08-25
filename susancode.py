9bba4be401fd322312a231482b701de1be26740c
swright <swright573@icloud.com>
2019-08-25 14:02:26 -0400

    Update susancode.py

--- susancode.py
+++ susancode.py
@@ -2,12 +2,15 @@
 Following along with TestandCode episode 80
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 
+def uppercase(sentence):
+    return(sentence.upper())    
 
 def outtext(sentence):
     print(sentence)
+    return
 
 def main():
     outtext('Hello world ... Susan here.')
